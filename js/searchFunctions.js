d55b8d93be6b5cc0f9718ba7833e2665c47f689c
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-04-06 17:52:44 +0200

    Added some styles

--- js/searchFunctions.js
+++ js/searchFunctions.js
@@ -3,14 +3,14 @@ searchInput.addEventListener('input', searchColors);
 
 function searchColors() {
     const searchQuery = searchInput.value.toLowerCase().replace(/\s+/g, "");
-    const colorDivs = document.querySelectorAll('.color');
+    const colorDivs = document.querySelectorAll('.colorbox');
 
     colorDivs.forEach(colorDiv => {
-        const name = colorDiv.querySelector('.color-name').innerText.toLowerCase();
-        const hex = colorDiv.querySelector('.color-hex').innerText.toLowerCase();
+        const name = colorDiv.querySelector('#colorname').innerText.toLowerCase();
+        const hex = colorDiv.querySelector('#colorhex').innerText.toLowerCase();
 
         if (name.includes(searchQuery) || hex.includes(searchQuery)) {
-            colorDiv.style.display = 'block';
+            colorDiv.style.display = 'flex';
         } else {
             colorDiv.style.display = 'none';
         }
