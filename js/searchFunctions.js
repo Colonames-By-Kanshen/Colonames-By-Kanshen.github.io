87bfee380e6aab422bf02895bed4b15a7a0e1fac
Richard Kužela <richard@kanshen.click>
2023-05-13 19:25:45 +0200

    Added some more colors

--- js/searchFunctions.js
+++ js/searchFunctions.js
@@ -5,6 +5,7 @@ function searchColors() {
     const searchQuery = searchInput.value.toLowerCase().replace(/\s+/g, "");
     const colorDivs = Array.from(document.querySelectorAll('.colorbox')).slice(0, -1);
     const Div404 = document.querySelector("div#ColorNotFound");
+    const BlurpleDivs = document.querySelectorAll("div.projectblurple");
 
     colorDivs.forEach(colorDiv => {
         const name = colorDiv.querySelector('#colorname').innerText.toLowerCase();
@@ -17,9 +18,19 @@ function searchColors() {
         }
     });
 
-    if (colorDivs.some(colorDiv => colorDiv.style.display === 'flex')) {
+    if (searchQuery.toLowerCase() === '#projectblurple') {
+        BlurpleDivs.forEach(blurpleDiv => {
+          blurpleDiv.style.display = 'flex';
+        });
+      } else {
+        BlurpleDivs.forEach(blurpleDiv => {
+          blurpleDiv.style.display = 'none';
+        });
+      }
+
+      if (colorDivs.some(colorDiv => colorDiv.style.display === 'flex') || BlurpleDivs.some(blurpleDiv => blurpleDiv.style.display === 'flex')) {
         Div404.style.display = 'none';
-    } else {
+      } else {
         Div404.style.display = 'flex';
-    }
+      }
 }
