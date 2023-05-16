4f7aec775f9191782b8fe5f9f9fc02db1f944647
Richard Kužela <richard@kanshen.click>
2023-05-16 15:09:03 +0200

    Revert "Added some more colors"
    This reverts commit 87bfee380e6aab422bf02895bed4b15a7a0e1fac.

--- js/searchFunctions.js
+++ js/searchFunctions.js
@@ -5,7 +5,6 @@ function searchColors() {
     const searchQuery = searchInput.value.toLowerCase().replace(/\s+/g, "");
     const colorDivs = Array.from(document.querySelectorAll('.colorbox')).slice(0, -1);
     const Div404 = document.querySelector("div#ColorNotFound");
-    const BlurpleDivs = document.querySelectorAll("div.projectblurple");
 
     colorDivs.forEach(colorDiv => {
         const name = colorDiv.querySelector('#colorname').innerText.toLowerCase();
@@ -18,19 +17,9 @@ function searchColors() {
         }
     });
 
-    if (searchQuery.toLowerCase() === '#projectblurple') {
-        BlurpleDivs.forEach(blurpleDiv => {
-          blurpleDiv.style.display = 'flex';
-        });
-      } else {
-        BlurpleDivs.forEach(blurpleDiv => {
-          blurpleDiv.style.display = 'none';
-        });
-      }
-
-      if (colorDivs.some(colorDiv => colorDiv.style.display === 'flex') || BlurpleDivs.some(blurpleDiv => blurpleDiv.style.display === 'flex')) {
+    if (colorDivs.some(colorDiv => colorDiv.style.display === 'flex')) {
         Div404.style.display = 'none';
-      } else {
+    } else {
         Div404.style.display = 'flex';
-      }
+    }
 }
